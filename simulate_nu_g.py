Mr23rd commit: fixed normalization for pdf and modifed z positive negative assignment

simulate_nu_g.py
@@ -132,11 +132,12 @@ def	simulate_positions(output_file= None,
 	selected_z=[]
 	selected_r=[]
 
+	even_num = 1
 	for	index_distribution	in	range(0,number_sources_used):
 		rng = np.random.RandomState(seed)	
 		random_val_used1	=	rng.uniform(0,	1)
 		z_selected	=	invCDF_vertical_height_z(random_val_used1)
-		if index_distribution<=number_sources_used/2:
+		if even_num==1:
 			selected_z.append(float(z_selected))
 		else:
 			selected_z.append(float(z_selected*(-1)))
@@ -144,6 +145,7 @@ def	simulate_positions(output_file= None,
 		random_val_used2	=	rng.uniform(0,	1)
 		r_selected	=	invCDF_distance_r(random_val_used1)
 		selected_r.append(float(r_selected))
+		even_num = even_num*-1
 	
 	np.random.shuffle(selected_r)
 	np.random.shuffle(selected_z)

sky_distribution.py
@@ -119,7 +119,7 @@ def ModifiedExponentialDist(param_used,r,z): # param_used = alpha,beta,h
 
 	r_solar=8.5
 	z_solar=0.015
-	maximum_pt = alpha*r_solar/beta # Found Using derivatve
+	maximum_pt = (alpha+1)*r_solar/beta # Found Using derivatve
 	
 	norm_r     = 1/(maximum_pt*((maximum_pt/r_solar)**alpha)*np.exp(-1.*beta*((maximum_pt-r_solar)/r_solar)))
 	