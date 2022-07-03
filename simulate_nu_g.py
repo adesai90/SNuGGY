Added fix: Integral of PDF now used for normalization.. And fixed PDF plotting for z.. Also added ipynb

simulate_nu_g.py
@@ -81,7 +81,7 @@ def	simulate_positions(output_file= None,
 
 	rng = np.random.RandomState(seed)
 
-	distribution_parameters_list = [r_0,z_0,alpha,beta,h]
+	distribution_parameters_list = [r_0,z_0,alpha,beta,h,r_min,r_max,z_min,z_max]
 	"""
 	Change	this	based	on	firesong	later
 

sky_distribution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
-import scipy
+import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 import matplotlib
 
@@ -22,7 +22,11 @@ def get_model(model_name,distribution_parameters_list,r,z,make_pdf_plot_location
     #								  z_0,    -------1
     #								  alpha,  -------2
     #								  beta,   -------3
-    #								  h]      -------4
+    #								  h,      -------4
+    #								  r_min,  -------5
+    #								  r_max,  -------6
+    #								  z_min,  -------7
+    #								  z_max]  -------8
 	#
 
     if not model_name in models.keys():
@@ -30,16 +34,27 @@ def get_model(model_name,distribution_parameters_list,r,z,make_pdf_plot_location
 
 
     if model_name =="exponential":
-    	param_used=[distribution_parameters_list[0],distribution_parameters_list[1]]
+    	param_used=[distribution_parameters_list[0],
+    				distribution_parameters_list[1],
+    				distribution_parameters_list[5],
+    				distribution_parameters_list[6],
+    				distribution_parameters_list[7],
+    				distribution_parameters_list[8]]
     elif model_name=="modified_exponential":
-    	param_used=[distribution_parameters_list[2],distribution_parameters_list[3],distribution_parameters_list[4]]
+    	param_used=[distribution_parameters_list[2],
+    				distribution_parameters_list[3],
+    				distribution_parameters_list[4],
+    				distribution_parameters_list[5],
+    				distribution_parameters_list[6],
+    				distribution_parameters_list[7],
+    				distribution_parameters_list[8]]
     else:
     	print("Wrong Model name; Should not get this message if notIMPLEMENTED ERROR code is working, Please check code")
     	exit()
 
     if make_pdf_plot_location!=None:
-    	x_arr_pdf_r = np.arange(0.001,15,0.1)
-    	x_arr_pdf_z = np.arange(0.001,3,0.1)
+    	x_arr_pdf_r = np.logspace(-3,2,100)
+    	x_arr_pdf_z = np.logspace(-3,1,100)
 
     	y_arr_pdf_r = []
     	y_arr_pdf_z = []
@@ -57,6 +72,7 @@ def get_model(model_name,distribution_parameters_list,r,z,make_pdf_plot_location
     	ax[1].plot(x_arr_pdf_z,y_arr_pdf_z)
     	ax[1].set_xlabel('Vertical Height (|z|) kpc')
     	ax[1].set_ylabel('PDF')
+    	#ax[1].set_yscale('log')
 
     	plt.savefig(make_pdf_plot_location+"pdf_used_for_distribution_%s.png"%(model_name),bbox_inches="tight")
 
@@ -80,18 +96,26 @@ def ExponentialSpatialDist(param_used,r,z): # param_used = r_0,z_0
 	#print("BASIC EXPONENTIAL MODEL IS USED")
 	r_0 = param_used[0]
 	z_0 = param_used[1]
+	r_min_int = param_used[2]
+	r_max_int = param_used[3]
+	z_min_int = param_used[4]
+	z_max_int = param_used[5]
 	r_solar=8.5
 	z_solar=0.015
 
+	# Set Upperlimits for integrals to avoid getting garbage
+	if r_max_int>=1000:
+		r_max_int=1000.0
+	if z_max_int>=1000:
+		z_max_int=1000.0
 	
-	maximum_pt = r_0 # Found Using derivatve
-
+	#maximum_pt = r_0 # Found Using derivatve
+	#norm_r     = 1/(maximum_pt*np.exp(-1.*(maximum_pt/r_0)))
 	
-	norm_r     = 1/(maximum_pt*np.exp(-1.*(maximum_pt/r_0)))
-	
-	
+	norm_r = 1/integrate.quad(lambda r_temp: r_temp * np.exp(-1.*r_temp/r_0), r_min_int, r_max_int)[0]
 	exp_dist = norm_r * r * np.exp(-1.*r/r_0)
-	norm_z     = 1 
+
+	norm_z     = 1/integrate.quad(lambda z_temp: np.exp(-1.* abs(z_temp)/z_0), z_min_int, z_max_int)[0]
 	exp_z    = norm_z * np.exp(-1.* abs(z)/z_0)
 	
 	try:
@@ -101,24 +125,14 @@ def ExponentialSpatialDist(param_used,r,z): # param_used = r_0,z_0
 			len(r)
 		except:
 			#print("R is a num")
-			1 #Just a placeholder
+			if r==0:
+				return (exp_z)
+			else:
+				return (exp_z*exp_dist)
 		else:
 			#print("R is an array")
 			if z==0:
 				return (exp_dist)
-			else:
-				return (exp_z*exp_dist)
-			
-
-		try:
-			len(z)
-		except:
-			#print("Z is a num")
-			1  #Just a placeholder
-		else:
-			#print("Z is an array")
-			if r==0:
-				return (exp_z)
 			else:
 				return (exp_z*exp_dist)
 	else:
@@ -146,17 +160,27 @@ def ModifiedExponentialDist(param_used,r,z): # param_used = alpha,beta,h
 	alpha = param_used[0]
 	beta  = param_used[1]
 	h     = param_used[2]
+	r_min_int = param_used[3]
+	r_max_int = param_used[4]
+	z_min_int = param_used[5]
+	z_max_int = param_used[6]
+
+	# Set Upperlimits for integrals to avoid getting garbage
+	if r_max_int>=1000:
+		r_max_int=1000.0
+	if z_max_int>=1000:
+		z_max_int=1000.0
 
 	r_solar=8.5
 	z_solar=0.015
-	maximum_pt = (alpha+1)*r_solar/beta # Found Using derivatve
-	
-	norm_r     = 1/(maximum_pt*((maximum_pt/r_solar)**alpha)*np.exp(-1.*beta*((maximum_pt-r_solar)/r_solar)))
-	
+	#maximum_pt = (alpha+1)*r_solar/beta # Found Using derivatve
+	#norm_r     = 1/(maximum_pt*((maximum_pt/r_solar)**alpha)*np.exp(-1.*beta*((maximum_pt-r_solar)/r_solar)))
+
+	norm_r = 1/integrate.quad(lambda r_temp: (r_temp*(r_temp/r_solar)**alpha)*np.exp(-1.*beta*((r_temp-r_solar)/r_solar)), r_min_int, r_max_int)[0]
 	exp_dist = norm_r * (r*(r/r_solar)**alpha)*np.exp(-1.*beta*((r-r_solar)/r_solar))
 	
 
-	norm_z     = 1 
+	norm_z     = 1/integrate.quad(lambda z_temp: np.exp(-1.* abs(z_temp)/h), z_min_int, z_max_int)[0]
 	exp_z    = norm_z*np.exp(-1.* abs(z)/h)
 
 	try:
@@ -166,24 +190,14 @@ def ModifiedExponentialDist(param_used,r,z): # param_used = alpha,beta,h
 			len(r)
 		except:
 			#print("R is a num")
-			1 #Just a placeholder
+			if r==0:
+				return (exp_z)
+			else:
+				return (exp_z*exp_dist)
 		else:
 			#print("R is an array")
 			if z==0:
 				return (exp_dist)
-			else:
-				return (exp_z*exp_dist)
-			
-
-		try:
-			len(z)
-		except:
-			#print("Z is a num")
-			1  #Just a placeholder
-		else:
-			#print("Z is an array")
-			if r==0:
-				return (exp_z)
 			else:
 				return (exp_z*exp_dist)
 	else: